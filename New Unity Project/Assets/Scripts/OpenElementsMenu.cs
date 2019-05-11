9cddd80e3f9eb175909e08b13812f208d6e678bc
mati ali <matiasali07@gmail.com>
2019-05-11 11:52:23 -0300

    Hide and show elements panel
    Does not change arrow direction yet

---
+++ New Unity Project/Assets/Scripts/OpenElementsMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenElementsMenu : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject button;
+
+    public void OpenPanel()
+    {
+        Animator animator = panel.GetComponent<Animator>();
+        
+        if(animator != null)
+        {
+            bool opened = animator.GetBool("open");
+            animator.SetBool("open", !opened);
+        }
+        if(button != null)
+        {
+            Text buttonText = button.GetComponentInChildren<Text>();
+            if(buttonText.text == "<")
+            {
+                buttonText.text = ">";
+            }
+            else
+            {
+                buttonText.text = "<";
+            }
+        }
+    }
+}
