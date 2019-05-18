3454f3136e292a812ec6e989653162869e2abce3
mati ali <matiasali07@gmail.com>
2019-05-18 19:14:00 -0300

    Particles can now be deleted
    También agregué contadores para controlar la cantidad de partículas que hay.

--- New Unity Project/Assets/Scripts/ParticleSpawner.cs
+++ New Unity Project/Assets/Scripts/ParticleSpawner.cs
@@ -7,6 +7,12 @@ public class ParticleSpawner : MonoBehaviour
     public GameObject[] nucleonPrefabs;
     [SerializeField]
     private Transform parent;
+    private Queue<GameObject> protonQueue = new Queue<GameObject>();
+    private Queue<GameObject> neutronQueue = new Queue<GameObject>();
+    private Queue<GameObject> electronQueue = new Queue<GameObject>();
+    private int protonCounter = 0;
+    private int neutronCounter = 0;
+    private int electronCounter = 0;
 
     public void SpawnNucleon(bool proton)
     {
@@ -17,11 +23,53 @@ public class ParticleSpawner : MonoBehaviour
         }
         GameObject prefab = nucleonPrefabs[index];
         GameObject spawn = Instantiate<GameObject>(prefab, parent);
+        if (proton)
+        {
+            protonQueue.Enqueue(spawn);
+            protonCounter++;
+        }
+        else
+        {
+            neutronQueue.Enqueue(spawn);
+            neutronCounter++;
+        }
     }
 
     public void SpawnElectron()
     {
         GameObject prefab = nucleonPrefabs[2];
         GameObject spawn = Instantiate<GameObject>(prefab, parent);
+        electronQueue.Enqueue(spawn);
+        electronCounter++;
+    }
+
+    public void RemoveNeutron()
+    {
+        if (neutronCounter != 0)
+        {
+            GameObject toDelete = neutronQueue.Dequeue();
+            Destroy(toDelete);
+            neutronCounter--;
+        }
+    }
+
+    public void RemoveProton()
+    {
+        if (protonCounter != 0)
+        {
+            GameObject toDelete = protonQueue.Dequeue();
+            Destroy(toDelete);
+            protonCounter--;
+        }
+    }
+
+    public void RemoveElectron()
+    {
+        if (electronCounter != 0)
+        {
+            GameObject toDelete = electronQueue.Dequeue();
+            Destroy(toDelete);
+            electronCounter--;
+        }
     }
 }
