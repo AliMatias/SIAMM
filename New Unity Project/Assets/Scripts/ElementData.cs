987bb15f2d7fc6ae63c4d959f185a813c77feecb
mati ali <matiasali07@gmail.com>
2019-05-19 18:08:45 -0300

    first attempt at creating db
    No funciona

---
+++ New Unity Project/Assets/Scripts/ElementData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ElementData
+{
+    private string name;
+    private int protons;
+    private int neutrons;
+    private int electrons;
+
+    public string Name { get => name; set => name = value; }
+    public int Protons { get => protons; set => protons = value; }
+    public int Neutrons { get => neutrons; set => neutrons = value; }
+    public int Electrons { get => electrons; set => electrons = value; }
+}
