7a5aa4222f40c92a313767e3e4b714c8f8444f5f
mati ali <matiasali07@gmail.com>
2019-05-20 18:23:58 -0300

    sqlite first implementation
    Un mini ejemplo con un select * from element.
    Solamente lista los nombres de los elementos que puse.

---
+++ New Unity Project/Assets/Scripts/DBManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System;
+using Mono.Data.Sqlite;
+
+public class DBManager : MonoBehaviour
+{
+    private string connectionString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/SIAMM.db";
+        getElement();
+    }
+    
+    private void getElement()
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand command = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM element";
+                command.CommandText = sqlQuery;
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader.GetString(1));
+                    }
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
