663bf2e5f42722db4dc90c20da02e397913f6be3
Miguel Gallo <midway2004@gmail.com>
2019-06-07 02:09:19 -0300

    1

--- New Unity Project/Assets/Scripts/LoadTper.cs
+++ New Unity Project/Assets/Scripts/LoadTper.cs
@@ -6,29 +6,34 @@ using UnityEngine.UI;
 public class LoadTper : MonoBehaviour
 {
 
-    public int rows = 4;
-    public int columns = 4;
-    private float buttonWidth;                                        //Change
-    private float buttonHeight;                                        //Change
-    public Button prefab;
+    //public int rows = 4;
+    //public int columns = 4;
+    //private float buttonWidth;                                        //Change
+    //private float buttonHeight;                                        //Change
+    //public Button prefab;
     private Button button;
 
     // Start is called before the first frame update
     void Start()
     {
-        RectTransform myRect = GetComponent<RectTransform>();        //Change
-        buttonHeight = myRect.rect.height / (float)rows;            //Change
-        buttonWidth = myRect.rect.width / (float)columns;            //Change
+        //RectTransform myRect = GetComponent<RectTransform>();        //Change
+        //buttonHeight = myRect.rect.height / (float)rows;            //Change
+        //buttonWidth = myRect.rect.width / (float)columns;            //Change
         GridLayoutGroup grid = this.GetComponent<GridLayoutGroup>();
-        grid.cellSize = new Vector2(buttonWidth, buttonHeight);
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                button = (Button)Instantiate(prefab);
-                button.transform.SetParent(transform, false);        //Change
-            }
-        }
+        //grid.cellSize = new Vector2(buttonWidth, buttonHeight);
+
+        Debug.Log(grid.constraintCount.ToString());
+
+        Debug.Log(grid.transform.childCount.ToString());
+
+        //for (int i = 0; i < rows; i++)
+        //{
+        //   for (int j = 0; j < columns; j++)
+        //  {
+        //     button = (Button)Instantiate(prefab);
+        //     button.transform.SetParent(transform, false);        //Change
+        // }
+        // }
     }
 
 
