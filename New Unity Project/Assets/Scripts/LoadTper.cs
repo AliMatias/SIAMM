ea372e95e1d48285a5e1a971ba4143ec1df89fde
Migue <midway2004@gmail.com>
2019-06-06 21:35:57 -0300

    Upload para casa

--- New Unity Project/Assets/Scripts/LoadTper.cs
+++ New Unity Project/Assets/Scripts/LoadTper.cs
@@ -31,9 +31,50 @@ public class LoadTper : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void GetColumnAndRow(GridLayoutGroup glg, out int column, out int row)
     {
-        
+        column = 0;
+        row = 0;
+
+        if (glg.transform.childCount == 0)
+            return;
+
+        //Column and row are now 1
+        column = 1;
+        row = 1;
+
+        //Get the first child GameObject of the GridLayoutGroup
+        RectTransform firstChildObj = glg.transform.
+            GetChild(0).GetComponent<RectTransform>();
+
+        Vector2 firstChildPos = firstChildObj.anchoredPosition;
+        bool stopCountingRow = false;
+
+        //Loop through the rest of the child object
+        for (int i = 1; i < glg.transform.childCount; i++)
+        {
+            //Get the next child
+            RectTransform currentChildObj = glg.transform.
+           GetChild(i).GetComponent<RectTransform>();
+
+            Vector2 currentChildPos = currentChildObj.anchoredPosition;
+
+            //if first child.x == otherchild.x, it is a column, ele it's a row
+            if (firstChildPos.x == currentChildPos.x)
+            {
+                column++;
+                //Stop couting row once we find column
+                stopCountingRow = true;
+            }
+            else
+            {
+                if (!stopCountingRow)
+                    row++;
+            }
+        }
     }
+
+
 }
