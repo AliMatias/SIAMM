1d4da6eed2fc6f61c4f29bc1184f92dd264c21d3
Ali Matias Gabriel <matias.ali@despegar.com>
2019-07-12 16:06:41 -0300

    chequeo y deshabilitación de botones correspondientes a átomos (solo script)

--- New Unity Project/Assets/Scripts/AtomManager.cs
+++ New Unity Project/Assets/Scripts/AtomManager.cs
@@ -16,10 +16,18 @@ public class AtomManager : MonoBehaviour
     //lista que indica elementos seleccionados en modo combinación
     private List<int> selectedAtoms = new List<int>();
     private PositionManager positionManager = PositionManager.Instance;
+    //lista de botones relevantes para los átomos
+    private GameObject[] atomButtons;
+    private GameObject plusAtomButton;
 
     //esto està porque lo necesita el "combination manager"
     //seguro cuando busque la combinaciòn posta, va a necesitar otra cosa, no esto.
     public List<int> SelectedAtoms { get => selectedAtoms; set => selectedAtoms = value; }
+
+    private void Awake(){
+        atomButtons = GameObject.FindGameObjectsWithTag("atomToggle");
+        activateDeactivateAtomButtons();
+    }
 
     //cambia entre modo combinación o normal
     public void SwitchCombineMode(){
@@ -67,6 +75,7 @@ public class AtomManager : MonoBehaviour
         if(withProton){
             spawnedAtom.SpawnNucleon(true, false);
         }
+        activateDeactivateAtomButtons();
     }
 
     //seleccionar átomo
@@ -187,6 +196,7 @@ public class AtomManager : MonoBehaviour
         //disponibilizo la posición denuevo
         positionManager.AvailablePositions[index] = true;
         Debug.Log("Se ha borrado el átomo " + index);
+        activateDeactivateAtomButtons();
     }
 
     //spawnear átomo seleccionado en la tabla periódica
@@ -199,6 +209,25 @@ public class AtomManager : MonoBehaviour
             newAtom.SpawnFromPeriodicTable(elementName);
         }else{
             Debug.Log("No hay más lugar para crear un nuevo átomo.");
+        }
+        activateDeactivateAtomButtons();
+    }
+
+    //activa-desactiva botones de acuerdo a la cant de átomos
+    private void activateDeactivateAtomButtons(){
+        bool status = true;
+        if(atomsList.Count.equals(0)){
+            status = false;
+        }
+
+        foreach(GameObject btn in atomButtons){
+            btn.interactable = status;
+        }
+
+        if(positionManager.NoPositionsLeft()){
+            plusAtomButton.interactable = false;
+        }else{
+            plusAtomButton.interactable = true;
         }
     }
 
