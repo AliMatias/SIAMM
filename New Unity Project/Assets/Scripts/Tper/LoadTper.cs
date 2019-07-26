c6b88030092729e74b6f48fc3e444960c6553462
Migue <midway2004@gmail.com>
2019-07-25 21:31:21 -0300

    ResponsiveTPER & Other changes

--- New Unity Project/Assets/Scripts/Tper/LoadTper.cs
+++ New Unity Project/Assets/Scripts/Tper/LoadTper.cs
@@ -12,7 +12,6 @@ public class LoadTper : MonoBehaviour
     private Button button;
     private QryElementos qryElement;
     private GridLayoutGroup glg;
-    private RectTransform parent;
     //estos parametros son estaticos en mi modelo! son estaticos
     private int row = 12;
     private int col = 23;
@@ -27,9 +26,10 @@ public class LoadTper : MonoBehaviour
         go.AddComponent<QryElementos>();
         qryElement = go.GetComponent<QryElementos>();
 
-        popup = FindObjectOfType<UIPopup>();
-        ResizeCells();
+        glg = this.GetComponent<GridLayoutGroup>();
 
+        popup = FindObjectOfType<UIPopup>();
+     
         //Recorro todas las celdas que tienen un game object
         for (int i = 0; i < glg.transform.childCount; i++)
         {
@@ -74,41 +74,6 @@ public class LoadTper : MonoBehaviour
                 textosObj[j].text = Convert.ToString(element.PesoAtomico);
             if (textosObj[j].name == "txtNombre")
                 textosObj[j].text = element.Simbol;
-        }
-    }
-
-    /*metodo para el dynamic size de las celdas del grid layout*/
-    private void ResizeCells()
-    {
-        //obtengo el objeto "rectangulo" para poder tomar las medias del PANEL!! ya que este script es componente del mismo
-        parent = gameObject.GetComponent<RectTransform>();
-
-        //obtengo la referencia al objeto grid layout -> el this es referencia sobre al PANEL
-        glg = this.GetComponent<GridLayoutGroup>();
-
-        //rezise de acuerdo a resolucion del PENEL! sobre el objeto grid layout
-        glg.cellSize = new Vector2(parent.rect.width / col, parent.rect.height / row);
-    }
-
-    /*metodo para el tamaño de las fuentes de los botones de la tabla periodica, se utiliza una proporcion de acuerdo
-    al tamaño ORIGINAL usado para el tamaño de una resoluion de pantalla de 15" */
-    private void ResizeFont(Button elem)
-    {
-        /*busco una proporcion aprox!!!*/
-        int xOriginal = 65;
-        int sizeActual;    
-        int sizeProporcionCell = Convert.ToInt32(glg.cellSize.x);
-
-        elem.GetComponent<RectTransform>().sizeDelta = new Vector2(glg.cellSize.y, glg.cellSize.x);
-
-        //obtengo la lista de objetos o coleccion de objetos de tipo TEXT que estan en los botones
-        Text[] textosObj = elem.GetComponentsInChildren<Text>();
-
-        //recorro todos los game object que contiene el boton, se podria hacer por orden de objetos, como estan creados en el boton
-        for (int j = 0; j < textosObj.Length; j++)
-        {
-            sizeActual = textosObj[j].fontSize;
-            textosObj[j].fontSize = (sizeProporcionCell * sizeActual) / xOriginal;
         }
     }
 
