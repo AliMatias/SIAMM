65c8982fdec330b90b72c9cb82602c0d0635144b
Migue <midway2004@gmail.com>
2019-07-31 20:12:15 -0300

    BugToolTip&DrawLabel

---
+++ New Unity Project/Assets/Scripts/UI/UIToolTipControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIToolTipControl : MonoBehaviour
+{
+    public CanvasGroup panelTper;
+    public CanvasGroup textToolTipCanvas;
+    public CanvasGroup toolTipCanvas;
+    public static bool flagTooltip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flagTooltip = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //si el panel de la tabla periodica esta inactivo.. siempre tiene que estar el toolipTEXT oculto.. soluciona BUG
+        //doble control
+        if (!panelTper.gameObject.activeSelf)
+        {
+            textToolTipCanvas.alpha = 0;
+            toolTipCanvas.alpha = 0;
+        }
+
+        if (!flagTooltip)
+        {
+
+            textToolTipCanvas.alpha = 0;
+            toolTipCanvas.alpha = 0;
+        }
+
+    }
+}
