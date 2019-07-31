bc6382392ba53721a22d5042e894a39e2c9cd558
mati ali <matiasali07@gmail.com>
2019-07-31 11:21:04 -0300

    insert posicion modificado y tipo de linea agregado

--- DB Scripts/INSERT_moleculas_element_posicion.sql
+++ DB Scripts/INSERT_moleculas_element_posicion.sql
@@ -1,8 +1,5 @@
-INSERT INTO moleculas_posicion3D_element (id_molecula,id_elemento,posX,posY,posZ,escala,conexion,tipo_conexion) VALUES (
-1,1,0.8,-0.61,0,0.15,3,1);
-INSERT INTO moleculas_posicion3D_element (id_molecula,id_elemento,posX,posY,posZ,escala,conexion,tipo_conexion) VALUES (
-1,1,-0.8,-0.61,0,0.15,3,1);
-INSERT INTO moleculas_posicion3D_element (id_molecula,id_elemento,posX,posY,posZ,escala,conexion,tipo_conexion) VALUES (
-1,8,0,0,0,0.3,0,0);
+INSERT INTO moleculas_posicion3D_element VALUES (1,1,1,0.8,-0.61,0,0.15,3,1,1);
+INSERT INTO moleculas_posicion3D_element VALUES (2,1,1,-0.8,-0.61,0,0.15,3,1,1);
+INSERT INTO moleculas_posicion3D_element VALUES (3,1,8,0,0,0,0.3,0,0,0);
 
 
