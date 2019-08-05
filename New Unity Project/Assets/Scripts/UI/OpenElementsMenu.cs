0e989af1f611f5625e4b4a7e9492335b40686346
Miguel Gallo <49215615+mgallo2019@users.noreply.github.com>
2019-08-04 22:59:27 -0300

    InteractivePanels_V1

--- New Unity Project/Assets/Scripts/UI/OpenElementsMenu.cs
+++ New Unity Project/Assets/Scripts/UI/OpenElementsMenu.cs
@@ -38,4 +38,33 @@ public class OpenElementsMenu : MonoBehaviour
             }
         }
     }
+
+    public void OpenPanelAtom()
+    {
+        //obtengo el animator del panel
+        Animator animator = panel.GetComponent<Animator>();
+        bool opened = false;
+        //si existe
+        if (animator != null)
+        {
+            //obtengo el booleano que dice si esta abierto, y lo cambio
+            opened = animator.GetBool("open");
+            animator.SetBool("open", !opened);
+        }
+        //si hay un botón
+        if (button != null)
+        {
+            //obtengo el texto del botón
+            Text buttonText = button.GetComponentInChildren<Text>();
+            //y lo cambio según corresponda
+            if (!opened)
+            {
+                buttonText.text = "<";
+            }
+            else
+            {
+                buttonText.text = ">";
+            }
+        }
+    }
 }
---
+++ New Unity Project/Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenu : MonoBehaviour
+{
+    string[] bottonNames = {"Abrir Proyecto","Nuevo Proyecto","Guardar", "Guardar Como", "Exportar", "Salir"};
+
+    float posX = -2 * Screen.width;
+    float posY = 0.05f * Screen.height;
+    float largo = 0.15f * Screen.width;
+    float altura = 0.5f * Screen.height;
+    //a donde tiene que terminar de posicionarse el menu luego de leslizar
+    float posXr = 0.01f * Screen.width;
+
+    float velocidad = 15f;
+
+    bool entrando = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(entrando && posXr != posX)
+        {
+            posX = Mathf.Lerp(posX, posXr, velocidad * Time.deltaTime);
+        }
+    }
+
+
+    void OnGUI()
+    {
+        posXr = 0.01f * Screen.width;
+        posY = 0.05f * Screen.height;
+        largo = 0.12f * Screen.width;
+        altura = 0.32f * Screen.height;
+
+        GUI.Box(new Rect(posX, posY, largo, altura), "Menú");
+
+        largo = 0.16f * Screen.width;
+
+        GUI.Box(new Rect(posX + 0.005f * Screen.width,
+                posY+ (0.03f + 0*0.1f) * Screen.height,
+                largo - 0.05f * Screen.width,
+                0.1f * Screen.height - 0.07f * Screen.height), bottonNames[0]);
+
+        for (int i=1; i<bottonNames.Length; i++)
+        {
+            GUI.Box(new Rect(posX +0.005f * Screen.width,
+                posY+ (0.03f + i*0.05f) * Screen.height,
+                largo - 0.05f * Screen.width,
+                0.1f * Screen.height - 0.07f * Screen.height), bottonNames[i]);
+           
+            //-0.07f -> ancho -0.05f el largo
+        }
+
+    }
+}
