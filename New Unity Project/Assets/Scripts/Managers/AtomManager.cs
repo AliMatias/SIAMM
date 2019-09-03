0819ef3bf052ee8e8eec5a724f06a71125b977fb
Migue <midway2004@gmail.com>
2019-09-03 17:44:30 -0300

    Revert "Revert "Merge branch 'develop' into AnimationCombination""
    This reverts commit 9e6378aca38e72cf8a7728d4a4dde9abaa260d36.

--- New Unity Project/Assets/Scripts/Managers/AtomManager.cs
+++ New Unity Project/Assets/Scripts/Managers/AtomManager.cs
@@ -41,11 +41,11 @@ public class AtomManager : MonoBehaviour
     //agregar nuevo átomo al espacio de trabajo
     public void NewAtom(bool withProton)
     {
-        //intento obtener una posición disponible random
+        //intento obtener una posición disponible 
         int position;
         try
         {
-            position = positionManager.ObtainRandomPositionIndex();
+            position = positionManager.GetFirstAvailablePositionIndex();
         }
         //si no hay mas posiciones disponibles, lo loggeo y me voy
         catch(NoPositionsLeftException nple)
@@ -226,7 +226,8 @@ public class AtomManager : MonoBehaviour
             btn.interactable = status;
         }
 
-        if(positionManager.NoPositionsLeft()){
+        if(positionManager.NoPositionsLeft())
+        {
             plusAtomButton.interactable = false;
         }else{
             plusAtomButton.interactable = true;
