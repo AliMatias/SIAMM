1ec60c07a071cbeede2663890457c2cf33eaf2a5
Migue <midway2004@gmail.com>
2019-10-04 15:41:55 -0300

    NewPackUIAssets
    Se agrega un nuevo Packui que reemplazara los usados por LEAN GUI (menos los paneles) asi mismo se modifica el BOTON del menu inf. para que ya quede con "animacion" antes quedo sin nada

---
+++ New Unity Project/Assets/Modern UI Pack/Scripts/CustomDropdown.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomDropdown : MonoBehaviour {
+
+	[Header("ANIMATORS")]
+	public Animator dropdownAnimator;
+
+	[Header("OBJECTS")]
+	public GameObject fieldTrigger;
+	public Text selectedText;
+	public Image selectedImage;
+
+	[Header("PLACEHOLDER")]
+	public string customText;
+	public Sprite customIcon;
+
+	[Header("SETTINGS")]
+	[Tooltip("IMPORTANT! EVERY DORPDOWN MUST HAVE A DIFFERENT ID")]
+	public int DropdownID = 0;
+	public bool customPlaceholder;
+	public bool rememberSelection = true;
+
+	//public bool darkTrigger = true;
+
+	private bool isOn;
+	private string inAnim = "In";
+	private string outAnim = "Out";
+
+	private string sText;
+	private string sImage;
+
+	void Start ()
+	{
+		if (rememberSelection == true)
+		{
+			sText = PlayerPrefs.GetString (DropdownID + "SelectedText");
+			sImage = PlayerPrefs.GetString (DropdownID + "SelectedImage");
+		}
+
+		if (customPlaceholder == true)
+		{
+			selectedText.text = customText;
+			selectedImage.sprite = customIcon;
+		}
+
+		else 
+		{
+			selectedText.text = sText;
+			//	selectedImage.sprite = 
+		}
+	}
+
+	public void Animate ()
+	{
+		if (isOn == true) 
+		{
+			dropdownAnimator.Play (outAnim);
+			isOn = false;
+			fieldTrigger.SetActive (false);
+		}
+
+		else
+		{
+			dropdownAnimator.Play (inAnim);
+			isOn = true;
+			fieldTrigger.SetActive (true);
+		}
+	}
+}
