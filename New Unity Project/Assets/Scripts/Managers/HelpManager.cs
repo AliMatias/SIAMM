516b9993b4d8214888f0cd8323d75d5942b75598
mati ali <matiasali07@gmail.com>
2019-10-18 20:00:49 -0300

    help

---
+++ New Unity Project/Assets/Scripts/Managers/HelpManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+    public GameObject help1;
+    public GameObject help2;
+    private int shown = 0;
+
+    public void switchHelp(){
+        switch(shown){
+            case 0:
+                helpPanel.SetActive(true);
+                help1.SetActive(true);
+                shown = 1;
+                break;
+            case 1:
+                help1.SetActive(false);
+                help2.SetActive(true);
+                shown = 2;
+                break;
+            case 2:
+                help2.SetActive(false);
+                helpPanel.SetActive(false);
+                shown = 0;
+                break;
+        }
+    }
+}
