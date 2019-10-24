a188e6727ae494b44e735c91b6c801820f70f5ac
Miguel Gallo <49215615+mgallo2019@users.noreply.github.com>
2019-10-24 03:35:50 -0300

    LastChanges
    Incluye: bloqueo de drag en lista scroleable de menus, bloqueo de cam cuando se quita el mouse del panel list y AUN se sigue escribiendo en el filtro (sino se movia.. como antes)

---
+++ New Unity Project/Assets/Scripts/UI/UIBlokingDrag.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class UIBlokingDrag : MonoBehaviour,  IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private ScrollRect ScrollRectParent;
+
+    public void Awake()
+    {
+        // find our ScrollRect parent
+        if (GetComponentInParent<ScrollRect>() != null)
+            ScrollRectParent = GetComponentInParent<ScrollRect>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        eventData.pointerDrag = null;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        eventData.pointerDrag = null;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        eventData.pointerDrag = null;
+    }
+}
+
+
+
+
